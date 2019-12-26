34b904cb349da0acca47f41bc622691e8c2a5552
doha <57834946+hero3131@users.noreply.github.com>
2019-12-26 23:10:11 +0100

    my second solo commit

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text FaceMoji;
+    public Text MojiText;
+
+    private Rigidbody rb;
+    private int count;
+
+void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetFaceMoji();
+        winText.text = "";
+        SetCountText();
+        
+
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pick up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText ();
+        }
+        else
+        {
+            other.gameObject.SetActive(false);
+            count = count + 2;
+            SetCountText();
+        }
+
+    }
+
+    void SetFaceMoji ()
+    {
+        if (count <=8)
+        {
+            countText.text = "Count (._.) : " + count.ToString();
+        }
+        else
+        { 
+            countText.text = "Count (^-^) : " + count.ToString();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text =  count.ToString();
+        SetFaceMoji();
+        if (count ==19)
+        {
+                winText.text = "Well Done !";
+         }
+     }
+    
+}
+
---
+++ Assets/script/TimerGameOver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerGameOver : MonoBehaviour
+{
+    int countDownStartValue = 30;
+    public Text timerUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDownTimer();
+    }
+
+    void countDownTimer()   
+    {
+        if (countDownStartValue > 0)
+        {
+            TimeSpan spanTime = TimeSpan.FromSeconds(countDownStartValue);
+            timerUI.text = "Timer : " + spanTime.Seconds;
+            countDownStartValue--;
+            Invoke("countDownTimer", 1.0f);
+        }
+        else
+        {
+            timerUI.text = "Timer : (o.O) !";
+        }
+    }
+}
